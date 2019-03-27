b332e84258d02f7ccad9575520b90011885f07e5
mahyar121 <mahyarhajibabaie@gmail.com>
2019-03-27 12:24:46 -0700

    Wrote the SQL query for the accounts table

--- src/main/resources/SQL/guild-website-poc-database.sql
+++ src/main/resources/SQL/guild-website-poc-database.sql
@@ -4,18 +4,36 @@ CREATE DATABASE  IF NOT EXISTS `guild_website_poc_app`;
 USE `guild_website_poc_app`;
 
 --
--- Table structure for table `users`
+-- Table structure for table `accounts`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-                       `accountId` int        NOT NULL AUTO_INCREMENT,
-                       `username` varchar(50) NOT NULL,
-                       `email` varchar(50)    NOT NULL,
-                       `hash` varchar(128)    NOT NULL,
-                       `salt` varchar(50)     NOT NULL,
-                       PRIMARY KEY (`accountId`)
-) ENGINE=InnoDB AUTO_INCREMENT=1
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts` (
+                       `account_id` int(11)        NOT NULL AUTO_INCREMENT,
+                       `username` varchar(50)      NOT NULL,
+                       `email` varchar(50)         NOT NULL,
+                       PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `hashSalts`
+--
+
+DROP TABLE IF EXISTS `hash_salts`;
+CREATE TABLE `hash_salts` (
+                          `hash_salt_account_id` int(11)  NOT NULL,
+                          `hash` varchar(128)             NOT NULL,
+                          `salt` varchar(50)              NOT NULL,
+                          PRIMARY KEY (`hash_salt_account_id`),
+                          CONSTRAINT `fk_hash_salt_account_id`
+                          FOREIGN KEY (`hash_salt_account_id`)
+                          REFERENCES `guild_website_poc_app`.`accounts`(`account_id`)
+                          ON UPDATE CASCADE
+                          ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `accounts` (username, email) VALUES ('abc', 'abc@gmail.com');
+
 
 
 
