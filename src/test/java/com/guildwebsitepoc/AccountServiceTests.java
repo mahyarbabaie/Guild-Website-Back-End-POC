69c67951147492c4697b7f2ab125b63b81e37ac2
mahyar121 <mahyarhajibabaie@gmail.com>
2019-12-24 16:41:01 -0800

    Added units tests for AccountService:

--- src/test/java/com/guildwebsitepoc/AccountServiceTests.java
+++ src/test/java/com/guildwebsitepoc/AccountServiceTests.java
@@ -1,71 +1,106 @@
 package com.guildwebsitepoc;
 
-import com.guildwebsitepoc.dao.AccountRepository;
 import com.guildwebsitepoc.model.Account;
 import com.guildwebsitepoc.service.AccountService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AccountServiceTests {
 
-    @Autowired
+    @MockBean
     private AccountService accountService;
 
-    @MockBean
-    private AccountRepository accountRepository;
+    // test variables
+    private Account testAccountBob = new Account("Bob", "bsmith@abc.com");
+    private Account testAccountJohn = new Account("John","jsmith@abc.com");
 
+    // validates retrieval of multiple accounts
     @Test
     public void getAccountsTest() {
         // Given
-        List<Account> accounts = new ArrayList<Account>();
-        accounts.add(new Account("John","jsmith@abc.com"));
-        accounts.add(new Account("Bob", "bsmith@abc.com"));
+        List<Account> expectedAccounts = new ArrayList<Account>();
+        expectedAccounts.add(testAccountBob);
+        expectedAccounts.add(testAccountJohn);
         // When
-        when(accountRepository.getAccount()).thenReturn(accounts);
+        when(accountService.getAccount()).thenReturn(expectedAccounts);
         // Then
-        assertEquals(accounts.size(), accountService.getAccount().size());
+        assertEquals(expectedAccounts.size(), accountService.getAccount().size());
     }
 
+    // validates retrieval of a single account
     @Test
     public void getAccountTest(){
         // Given
-        Account account = new Account("Bob", "bsmith@abc.com");
+        Account expectedAccount = testAccountBob;
         // When
-        when(accountRepository.getAccount(1)).thenReturn(account);
+        when(accountService.getAccount(1)).thenReturn(expectedAccount);
         // Then
-        assertEquals(account, accountService.getAccount(1));
+        assertEquals(expectedAccount, accountService.getAccount(1));
     }
 
+    // validates adding a single account
     @Test
-    public void addAccountTest(){
+    public void addAccountTest() {
         // Given
-        Account account = new Account ("Bob", "bsmith@abc.com");
-        account.setPasswordHash("password");
+        Account expectedAccount = testAccountBob;
+        expectedAccount.setPasswordHash("password");
         // When
         doAnswer((Answer) -> {
             System.out.println("Adding an Account");
-            assertEquals(account, Answer.getArgument(0));
+            // Then
+            Account actualAccount = Answer.getArgument(0);
+            assertEquals(expectedAccount.getUsername(), actualAccount.getUsername());
+            assertEquals(expectedAccount.getEmail(), actualAccount.getEmail());
+            assertEquals(expectedAccount.getAccountId(), actualAccount.getAccountId());
             return null;
-        }).when(accountRepository).addAccount(any(Account.class));
-        accountService.addAccount(account);
+        }).when(accountService).addAccount(any(Account.class));
+        accountService.addAccount(expectedAccount);
+    }
+
+    // validates updating a account
+    @Test
+    public void updateAccountTest() {
+        // Given
+        Account expectedAccount = testAccountBob;
+        // When
+        doAnswer((Answer) -> {
+            System.out.println("Updating an Account");
+            // Then
+            Account actualAccount = Answer.getArgument(0);
+            assertEquals(expectedAccount.getUsername(), actualAccount.getUsername());
+            assertEquals(expectedAccount.getEmail(), actualAccount.getEmail());
+            assertEquals(expectedAccount.getAccountId(), actualAccount.getAccountId());
+            return null;
+        }).when(accountService).updateAccount(any(Account.class));
+        accountService.updateAccount(expectedAccount);
+    }
+
+    // validates deleting an account
+    @Test
+    public void deleteAccountTest() {
+        // Given
+        Account expectedAccount = testAccountBob;
+        expectedAccount.setAccountId(1);
+        // When
+        doAnswer((Answer) -> {
+            System.out.println("Deleting an Account");
+            // Then
+            Integer actualAccountId = Answer.getArgument(0);
+            assert(expectedAccount.getAccountId() == actualAccountId);
+            return null;
+        }).when(accountService).deleteAccount(anyInt());
+        accountService.deleteAccount(1);
     }
 
 }
