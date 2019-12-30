fbcedd9fd6572c864ae94eaa74a7f31f90e4878e
mahyar121 <mahyarhajibabaie@gmail.com>
2019-12-29 16:08:36 -0800

    - Completed initial GuildApplicationRepositoryImpl

--- src/main/java/com/guildwebsitepoc/dao/GuildApplicationRepositoryImpl.java
+++ src/main/java/com/guildwebsitepoc/dao/GuildApplicationRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.guildwebsitepoc.dao;
 
+import com.guildwebsitepoc.model.Account;
 import com.guildwebsitepoc.model.GuildApplication;
 import com.guildwebsitepoc.utility.GuildApplicationConstants;
+import com.guildwebsitepoc.utility.mapper.AccountMapper;
 import com.guildwebsitepoc.utility.mapper.GuildApplicationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -25,11 +27,10 @@ public class GuildApplicationRepositoryImpl implements GuildApplicationRepositor
     public List<GuildApplication> getPendingGuildApplication() {
         try {
             String sql = "SELECT * FROM " + guildApplicationTable + " WHERE application_status = ?";
-            List<GuildApplication> pendingGuildApplicationList = jdbcTemplate.query(
+            return jdbcTemplate.query(
                     sql,
                     new Object[]{GuildApplicationConstants.ApplicationStatus.PENDING.getValue()},
                     new GuildApplicationMapper());
-            return pendingGuildApplicationList;
         } catch (EmptyResultDataAccessException e) {
             return null;
         }
@@ -37,26 +38,103 @@ public class GuildApplicationRepositoryImpl implements GuildApplicationRepositor
 
     @Override
     public List<GuildApplication> getDeclinedGuildApplication() {
-        return null;
+        try {
+            String sql = "SELECT * FROM " + guildApplicationTable + " WHERE application_status = ?";
+            return jdbcTemplate.query(
+                    sql,
+                    new Object[]{GuildApplicationConstants.ApplicationStatus.DECLINED.getValue()},
+                    new GuildApplicationMapper());
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public List<GuildApplication> getAcceptedGuildApplication() {
-        return null;
+        try {
+            String sql = "SELECT * FROM " + guildApplicationTable + " WHERE application_status = ?";
+            return jdbcTemplate.query(
+                    sql,
+                    new Object[]{GuildApplicationConstants.ApplicationStatus.ACCEPTED.getValue()},
+                    new GuildApplicationMapper());
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public GuildApplication getGuildApplication(String applicationId) {
-        return null;
+        try {
+            String sql = "SELECT * FROM " + guildApplicationTable + " WHERE application_id = ?";
+            return jdbcTemplate.queryForObject(sql, new Object[]{applicationId}, new GuildApplicationMapper());
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
+    // TODO make this idempotent
     @Override
     public GuildApplication addGuildApplication(GuildApplication guildApplication) {
-        return null;
+        String sql = "INSERT INTO " +
+                guildApplicationTable +
+                " (" +
+                "application_id," +
+                "email," +
+                "battle_tag," +
+                "character_name," +
+                "character_class," +
+                "character_spec," +
+                "realm," +
+                "raid_preference," +
+                "raid_experience," +
+                "heroic_raid_preference," +
+                "mythic_raid_preference," +
+                "casual_preference," +
+                "casual_activities," +
+                "mythic_plus_preference," +
+                "mythic_plus_goals," +
+                "pvp_preference," +
+                "arena_preference," +
+                "battlegrounds_preference," +
+                "world_pvp_preference," +
+                "pvp_goals," +
+                "about_yourself," +
+                "application_status," +
+                "application_date" +
+                ") VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+        jdbcTemplate.update(
+                sql,
+                guildApplication.getApplicationId(),
+                guildApplication.getEmail(),
+                guildApplication.getBattleTag(),
+                guildApplication.getCharacterName(),
+                guildApplication.getCharacterClass(),
+                guildApplication.getCharacterSpec(),
+                guildApplication.getRealm(),
+                guildApplication.getRaidPreference(),
+                guildApplication.getRaidExperience(),
+                guildApplication.getHeroicRaidPreference(),
+                guildApplication.getMythicRaidPreference(),
+                guildApplication.getCasualPreference(),
+                guildApplication.getCasualActivities(),
+                guildApplication.getMythicPlusPreference(),
+                guildApplication.getMythicPlusGoals(),
+                guildApplication.getPvpPreference(),
+                guildApplication.getArenaPreference(),
+                guildApplication.getBattlegroundsPreference(),
+                guildApplication.getWorldPvpPreference(),
+                guildApplication.getPvpGoals(),
+                guildApplication.getAboutYourself(),
+                guildApplication.getApplicationStatus(),
+                guildApplication.getApplicationDate());
+
+        return guildApplication;
     }
 
     @Override
     public void deleteGuildApplication(String applicationId) {
-
+        String sql = "DELETE FROM " + guildApplicationTable + " WHERE application_id=?";
+        jdbcTemplate.update(sql, applicationId);
     }
 }
